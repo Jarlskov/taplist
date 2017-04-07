e1a13a1aa50687af7ca2e173e97066d8428d664c
Jesper Jarlskov <jesper@jarlskov.dk>
2017-04-07 22:18:32 +0200

    Run listings update every 10 min

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,6 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        // Commands\Inspire::class,
         Commands\UpdateTaplistingsCommand::class,
     ];
 
@@ -25,7 +24,6 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->command('listings:update')->everyTenMinutes();
     }
 }
