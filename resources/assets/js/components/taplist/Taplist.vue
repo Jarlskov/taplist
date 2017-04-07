891fd225c767a5db8851b679189d8314788dbf47
Jesper Jarlskov <jesper@jarlskov.dk>
2017-04-07 21:49:57 +0200

    Move fronted to webpack and Vue

---
+++ resources/assets/js/components/taplist/Taplist.vue
@@ -0,0 +1,42 @@
+<script>
+export default {
+    props: {
+        bars: {
+            required: true
+        },
+        listings: {
+            required: true
+        }
+    }
+}
+</script>
+
+<template>
+    <div>
+        <label for="barlist">Select bar: </label>
+        <select id="barlist">
+            <option selected value="">All</option>
+            <option v-for="bar in bars">{{ bar.name }}</option>
+        </select>
+        <table id="listings">
+            <thead>
+                <tr>
+                    <th>Bar</th>
+                    <th>Tap</th>
+                    <th>Name</th>
+                    <th>Brewery</th>
+                    <th>Rating</th>
+                </tr>
+            </thead>
+            <tbody> 
+                <tr v-for="listing in listings">
+                    <td>{{ listing.bar.name }}</td>
+                    <td>{{ listing.tap_name }}</td>
+                    <td>{{ listing.beer.name }}</td>
+                    <td>{{ listing.beer.brewery }}</td>
+                    <td>{{ listing.beer.ratebeeroverallrating }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
